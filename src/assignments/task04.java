e71c748cae55cc2652876dc3586a5d833ebf7ece
safia-ibrahimi <safia.ibrahimi.999@gmail.com>
2022-06-01 18:14:55 -0500

    2nd commit

--- src/assignments/task04.java
+++ src/assignments/task04.java
@@ -3,7 +3,25 @@ package assignments;
 public class task04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int [][] numbers= {
+				{12, 2, 26, 100, 3, 5, 13},
+				{1,11, 101, 33, 122, 8, 15},
+		};
+		
+		
+		
+		for (int even=0; even<numbers.length; even++) {
+			
+			for (int col=0; col<numbers[even].length; col++) {
+				
+				if (numbers[even][col]%2==0) {
+			
+			System.out.print(numbers[even][col]+" ");
+				}
+			
+			}
+		}
 
 	}
 
